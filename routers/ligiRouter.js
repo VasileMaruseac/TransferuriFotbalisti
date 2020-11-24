0e3e92880ac8eec9889ec1036089ebdb2632df80
Vasile Maruseac <vasile.maruseac@assist.ro>
2020-11-24 19:01:49 +0200

    routers

---
+++ routers/ligiRouter.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const router = express.Router()
+var Sequelize = require("sequelize");
+var initModels = require("../models/init-models").initModels; 
+// sequelize-auto -o "./src/modelss" -d citygo -h localhost -u root -p 3306 -x root -e mysql
+const mysql = {
+    dbname: 'proiectppaw',
+    user: 'vasi',
+    pass: '@DminTest123!',
+    options: { dialect: 'mysql', port: 3306 }
+  };
+var sequelize = new Sequelize(mysql.dbname, mysql.user, mysql.pass, mysql.options);
+var models = initModels(sequelize);
+var ligi = models.ligi;
+
+
+router.post("/createLiga", async(req, res) => {
+    try{
+        const s = await ligi.create(req.body).catch("err");
+        res.send("Created")
+    } catch(err){
+        res.send(err.message)
+    }
+})
+
+router.get("/getLigi", async(req, res)=> {
+    try{
+        const s = await ligi.findAll()
+        res.send(s)
+    }catch(err){
+        res.send(err.message)
+    }
+})
+
+router.get("/getLiga", async(req, res)=> {
+    try{
+        const x = req.body
+        console.log(x)
+        const s = await ligi.findAll({where: x})
+        res.send(s)
+    }catch(err){
+        res.send(err.message)
+    }
+})
+
+module.exports = router
